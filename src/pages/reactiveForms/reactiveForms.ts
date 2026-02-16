ac9aa9bd70d10f19c6a42099cce2b441dfce74fc
byOscarPINE <emanuelsant768@gmail.com>
2026-02-16 09:49:50 -0600

    16/02/2026

--- src/pages/reactiveForms/reactiveForms.ts
+++ src/pages/reactiveForms/reactiveForms.ts
@@ -16,6 +16,50 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class ReactiveForms { 
   fb = inject(FormBuilder)
   formProduct!: FormGroup;
+  formEmpleado!: FormGroup;
+
+  validatorMessage = {
+    name: [
+      { type: 'required', message: 'El nombre es requerido' }
+    ],
+    price: [
+      { type: 'required', message: 'El precio es requerido' },
+      { type: 'min', message: 'El precio debe ser mayor a 0' }
+    ],
+    stock: [
+      { type: 'required', message: 'El stock es requerido' },
+      { type: 'min', message: 'El stock debe ser mayor a 0' }
+    ],
+    perfil:[
+      { type: 'required', message: 'El perfil es requerido' },
+      { type: 'minlength', message: 'El perfil debe tener al menos 30 caracteres' }
+    ],
+    puesto:[
+      { type: 'required', message: 'El puesto es requerido' },
+    ],
+    edad:[
+      { type: 'required', message: 'La edad es requerida' },
+      { type: 'min', message: 'La edad debe ser mayor a 18' },
+      { type: 'max', message: 'La edad debe ser menor a 70' }
+    ],
+    email:[
+      { type: 'required', message: 'El email es requerido' },
+    ],
+    generate:[
+      { type: 'required', message: 'La cantidad a generar es requerida' },
+      { type: 'min', message: 'La cantidad a generar debe ser mayor a 1000' },
+    ]
+  }
+
+  getErrorMessage(control: string){
+    const errors = this.formEmpleado.get(control)?.errors;
+    if(errors){
+      const firstKey = Object.keys(errors)[0];
+      const validator = this.validatorMessage[control as keyof typeof this.validatorMessage].find(v => v.type === firstKey);
+      return validator ? validator.message : '';
+    }
+    return '';
+  }
 
   constructor() {
     this.formProduct = this.fb.group({
@@ -24,10 +68,28 @@ export class ReactiveForms {
       price: [,Validators.required],
       stock: [,Validators.required]
     })
+    this.formEmpleado = this.fb.group({
+      name: ['',[Validators.required, Validators.minLength(3)]],
+      perfil: ['', [Validators.required, Validators.minLength(30)]],
+      puesto: ['',Validators.required],
+      edad: [,[Validators.required,Validators.min(18),Validators.max(70)]],
+      email: [,Validators.required],
+      generate: [,[Validators.required, Validators.min(1000)]],
+    })
   }
 
   saveProduct(){
-    console.log(this.formProduct.value);
+    if(this.formProduct.invalid){
+      this.formProduct.markAllAsTouched()
+      return;
+    }else{
+      console.log(this.formProduct.value);
+    }
   }
+
+  // setErrors(control:String){
+  //   return this.formProduct.get(control)?.errors &&
+  //   this.formProduct.get(control)?.touched
+  // }
   
 }
